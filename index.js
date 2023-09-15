7632450574e02ecda4495a0c2eb2bfb68ed8a096
afyqeyzar <norafyqe@gmail.com>
2023-09-15 22:11:30 +0100

    added http methods to index.js

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 const http = require("http");
 const fs = require("fs");
+
+const hostName = "localhost";
 const port = 8080;
 
 const server = http.createServer((req, res) => {
-  res.write("Hello Node");
-  res.end();
+  const myURL = new URL(req.url, `http://${hostName}:${port}/`);
+  let fileName = "";
+  myURL.pathname === "/" ? (fileName = `index`) : (fileName = myURL.pathname);
+
+  fs.readFile(`./${fileName}.html`, (err, data) => {
+    if (err) {
+      res.writeHead(404, { "Content-Type": "text/html" });
+      res.write(
+        fs.readFileSync("./404.html", (err, data) => {
+          if (err) throw err;
+          return data;
+        })
+      );
+      return res.end();
+    } else {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(data);
+      return res.end();
+    }
+  });
 });
 
 server.listen(port, (error) => {
